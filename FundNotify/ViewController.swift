a57b1cfbffef619e40eb0d8e95887e2d161749a6
Keita Suzuki <keita.suzuki@careritztc.co.jp>
2024-11-09 15:55:23 +0900

    Initial Commit

---
+++ FundNotify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FundNotify
+//
+//  Created by Keita Suzuki on 2024/11/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FundNotifyTests/FundNotifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  FundNotifyTests.swift
+//  FundNotifyTests
+//
+//  Created by Keita Suzuki on 2024/11/09.
+//
+
+import Testing
+@testable import FundNotify
+
+struct FundNotifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
